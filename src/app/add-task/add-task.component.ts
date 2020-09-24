f7d8228c81ccadfbc1e92d6441126f20ee5fc508
praveen-barawal <praveen.in.7@gmail.com>
2020-09-24 20:20:22 +0530

    2 files modified

---
+++ src/app/add-task/add-task.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit, ViewChild } from "@angular/core";
+import { angularAnimation } from "../Animation/animation";
+import Typewriter from 't-writer.js';
+
+@Component({
+  selector: "add-task",
+  templateUrl: "./add-task.component.html",
+  animations: [angularAnimation]
+})
+export class AddTaskComponent {
+  @Input() isChecked: boolean;
+  @Input() className: string;
+  @ViewChild("tw") typewriterElement;
+  @ViewChild("tw2") typewriterElement2;
+  @ViewChild("tw3") typewriterElement3;
+
+  show = true;
+  hideLabel = false;
+
+  ngOnInit() {
+    setTimeout(() => (this.hideLabel = true), 1000);
+
+    const target = this.typewriterElement.nativeElement;
+    const writer = new Typewriter(target, {
+      loop: false,
+      typeColor: "#5d5581"
+    });
+
+    writer
+      .type(`Make animation`)
+      .rest(500)
+      .start();
+  }
+}
