efad9cad0facb4d10a0d6ae32b8c26f94d5cabb6
praveen-barawal <praveen.in.7@gmail.com>
2020-09-24 20:36:57 +0530

    4 files modified

--- src/app/animation/animation.ts
+++ src/app/animation/animation.ts
@@ -3,76 +3,24 @@ import {
   state,
   style,
   transition,
-  animate,
+  animate
 } from "@angular/animations";
 
 export const AngularAnimation = [
-  trigger("slideInOutUp", [
-    transition(":enter", [
+  trigger("fadeOut", [
+    state(
+      "void",
       style({
-        transform: "translateY(-50%)",
         opacity: 0
-      }),
-      animate( 
-        "400ms ease-in",
-        style({
-          transform: "translateY(-100%)",
-          opacity: 0
-        })
-      )
-    ]),
-    transition(":leave", [
-      style({ zIndex: "0" }),
-      animate("600ms ease-out", style({ transform: "translateY(-80%)" }))
-    ])
-  ]),
-
-  trigger("slideInOutDown", [
-    transition(":leave", [
-      style({ zIndex: "1" }),
-      animate("400ms ease-out", style({ transform: "translateY(100%)" })),
-      transition(":enter", [
-        style({
-          transform: "translateY(-20%)",
-          zIndex: "1",
-          opacity: 0
-        }),
-        animate(
-          "600ms ease-in-out",
-          style({
-            transform: "translateY(-200%)",
-            zIndex: "1",
-            opacity: 0
-          })
-        )
-      ])
-    ])
-  ]),
-
-  trigger("slideInOut", [
-    transition(":enter", [
-      style({ transform: "translateY(100%)", opacity: 0 }),
-      animate(
-        "600ms ease-in",
-        style({ transform: "translateY(0%)", opacity: 1 })
-      )
-    ]),
-    transition(":leave", [
-      style({ transform: "translateY(0%)", opacity: 1 }),
-      animate("0ms ease-in", style({ transform: "translateY(0%)", opacity: 0 }))
-    ])
-  ]),
-
-  trigger("slideView", [
-    state("true", style({ transform: "translateY(100%)", opacity: 0 })),
-    state("false", style({ transform: "translateY(0)", opacity: 1 })),
-    transition(
-      "0 => 1",
-      animate("500ms", style({ transform: "translateY(0)", opacity: 1 }))
+      })
     ),
-    transition(
-      "1 => 1",
-      animate("500ms", style({ transform: "translateY(100%)", opacity: 0 }))
-    )
+    transition(":leave", animate(600, style({ opacity: 2 })))
+  ]),
+  trigger("Leave", [
+    state("flyIn", style({ transform: "translateX(0)" })),
+    transition(":enter", [
+      style({ transform: "translateX(-100%)" }),
+      animate("2s 100ms ease-in")
+    ])
   ])
 ];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { trigger, style, animate, transition, state } from '@angular/animations';
-import Typewriter from 't-writer.js';
+import { Component, OnInit, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ],
-  animations: [ 
-    trigger('fadeOut', [
-  state('void', style({
-    opacity: 0
-  })),
-   transition(':leave',
-        animate(600, style({opacity: 2})))
-   ]),
-    trigger('Leave', [
-      state('flyIn', style({ transform: 'translateX(0)' })),
-      transition(':enter', [
-      style({ transform: 'translateX(-100%)' }),
-      animate('2s 100ms ease-in') 
-    ]),
-  ],
-)]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent implements OnInit  {
-  @ViewChild('tw') typewriterElement;
-  @ViewChild('tw2') typewriterElement2;
-  @ViewChild('tw3') typewriterElement3;
-
-  show = true;
-  hideLabel = false;
-
-  ngOnInit() {
-    setTimeout(() => 
-    this.hideLabel =true
-    , 1000);
-    
-    const target = this.typewriterElement.nativeElement
-    const writer = new Typewriter(target, {
-      loop: false,
-      typeColor: '#5d5581'
-    })
-
-    writer
-      .type(`Make animation`)
-      .rest(500)
-      .start()
-
-  }
-  }
-
+export class AppComponent implements OnInit {
+  ngOnInit() {}
+}
